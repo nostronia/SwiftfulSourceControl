8a6aca54fd44ed9b5fe37b428602b47020d7fe51
Virgil Alaniz <nostronia@yahoo.com>
2024-06-04 23:52:05 -0600

    Added HomeView file

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Administrator on 6/4/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen 2!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
