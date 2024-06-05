a9a9d537e4c6235b9b7ece00065de67c036a71bf
Virgil Alaniz <nostronia@yahoo.com>
2024-06-05 10:02:48 -0600

    [Clean]  Added commit messages documentation

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -5,6 +5,28 @@
 //  Created by Administrator on 6/3/24.
 //
 
+
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [FEATURE] Description of feature
+ 
+ BUG IN PRODUCTION:
+ [PATCH] Description of bug
+ 
+ BUG NOT IN PRODUCTION:
+ [BUG] Description of bug
+ 
+ MUNDANE TASKS:
+ [CLEAN] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
+
+
 import SwiftUI
 
 struct ContentView: View {
